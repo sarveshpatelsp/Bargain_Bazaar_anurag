af0127f87bf0cde477a9403cb023fe0c300bc97b
Anuragroy1 <anuragroy.xworkx@gmail.com>
2023-10-10 14:22:14 +0530

    I started this app on 9/10/23 and add splash screen

---
+++ app/src/main/java/com/example/bargainbazar/splashActivity.java
@@ -0,0 +1,24 @@
+package com.example.bargainbazar;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+public class splashActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splashscreen);
+        new Handler().postDelayed(new Runnable(){
+            @Override
+            public void run(){
+            Intent intent = new Intent( splashActivity.this, MainActivity.class);
+            startActivity(intent);
+            finish();
+        }
+    }, 3000);
+    }
+}
