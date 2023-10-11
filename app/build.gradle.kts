bacc9e4b262b9600bdf3b8b1db6b5a5796934761
Anuragroy1 <anuragroy.xworkx@gmail.com>
2023-10-11 19:22:28 +0530

    I started this app on 9/10/23 and add splash screen and navigation is done but page are not changing

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -42,9 +42,10 @@ dependencies {
     implementation("com.google.android.gms:play-services-maps:18.1.0")
     implementation("androidx.lifecycle:lifecycle-livedata-ktx:2.6.2")
     implementation("androidx.lifecycle:lifecycle-viewmodel-ktx:2.6.2")
+    implementation("androidx.navigation:navigation-fragment:2.5.3")
+    implementation("androidx.navigation:navigation-ui:2.5.3")
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
     implementation("pl.droidsonroids.gif:android-gif-drawable:1.2.28")
-    implementation("com.google.android.material:material:1.5.0")
 }
--- app/src/main/java/com/example/bargainbazar/MainActivity.java
+++ app/src/main/java/com/example/bargainbazar/MainActivity.java
@@ -1,14 +1,49 @@
 package com.example.bargainbazar;
 
-import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
+
+import com.example.bargainbazar.databinding.ActivityMainBinding;
+
+
 public class MainActivity extends AppCompatActivity {
 
+    ActivityMainBinding binding;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        binding=ActivityMainBinding.inflate(getLayoutInflater());
         setContentView(R.layout.activity_main);
+//        replaceFragment(new HomeFragment());
+
+        binding.bottomNavigationView.setOnItemSelectedListener(item -> {
+            switch (item.getItemId()){
+
+                case R.id.home:
+                    replaceFragment(new HomeFragment());
+                    break;
+                case R.id.myorder:
+                     replaceFragment(new MyorderFragment());
+                    break;
+                case R.id.chats:
+                     replaceFragment(new ChatFragment());
+                    break;
+                case R.id.user:
+                    replaceFragment(new UserFragment());
+                    break;
+            }
+            return true;
+        });
+    }
+    private void replaceFragment(Fragment fragment){
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+        fragmentTransaction.replace(R.id.frame_layout, fragment);
+        fragmentTransaction.commit();
     }
 }
